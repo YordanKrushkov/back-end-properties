ad42ad937b3aed1fcce7e8ed01248f785d2cfcb5
Yordan Krushkov <yordankrushkov@gmail.com>
2021-03-27 12:33:55 +0000

    Update config.js

--- Config/config.js
+++ Config/config.js
@@ -2,11 +2,15 @@ const env = process.env.NODE_ENV || 'development'
 
 const config = {
     development: {
-        port: process.env.PORT,
+        port: process.env.PORT ||80,
         dbURL: process.env.DB_URL,
         privateKey: process.env.PRIVATE_KEY
     },
-    production: {}
+    production: {
+        port: process.env.PORT,
+        dbURL: process.env.DB_URL,
+        privateKey: process.env.PRIVATE_KEY
+    }
 };
 
 module.exports = config[env];
